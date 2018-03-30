c2edc86bb127359df0327ba7cad8408f374f9710
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-03-30 11:04:27 +0900

    add basic component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Route } from 'react-router-dom';
+import HomePage from './component/pages/HomePage';
+import LoginPage from './component/pages/LoginPage';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route path="/" exact component={HomePage}/>
+        <Route path="/login" exact component={LoginPage}/>
       </div>
     );
   }
---
+++ src/component/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom'
+class LoginPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+        {/* <Link to="/login">Login</Link> */}
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
