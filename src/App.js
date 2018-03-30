309972540702b0b3ec2d9493d89527f37e1f376b
VAN HUYNH VIEN DONG <dongvhv@caters.co.jp>
2018-03-30 14:29:27 +0900

    add redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
+import {createStore,applyMiddleware} from 'react-redux'
 import HomePage from './component/pages/HomePage';
 import LoginPage from './component/pages/LoginPage';
+
+const store = createStore
 class App extends Component {
   render() {
     return (
